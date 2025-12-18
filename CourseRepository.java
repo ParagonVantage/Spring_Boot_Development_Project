6c2b80fbe5db435e86d2468c37b4cf53526bc154
Paragon Vantage <agneya2208@gmail.com>
2025-12-17 16:52:21 -0800

    Reorder import statements in CourseRepository

--- CourseRepository.java
+++ CourseRepository.java
@@ -1,7 +1,6 @@
-package com.example.springdemo.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.springdemo.model.Course;
 
 public interface CourseRepository extends JpaRepository<Course, Long> { }
+
