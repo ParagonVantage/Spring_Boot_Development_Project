353eb766b8b54267cbbbd9310f2621136fa6aad4
Paragon Vantage <agneya2208@gmail.com>
2025-12-17 16:54:13 -0800

    Reorder import statements in StudentRepository

--- StudentRepository.java
+++ StudentRepository.java
@@ -1,7 +1,6 @@
-package com.example.springdemo.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.springdemo.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> { }
+
